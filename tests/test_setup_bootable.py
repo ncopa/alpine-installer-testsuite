024d9bf42041bc98085d79af275fd35fb3824cec
Natanael Copa <ncopa@alpinelinux.org>
2022-05-10 17:01:26 +0200

    test_setup_bootable: new tests
    test create a boot disk with setup-bootable, boot it and install alpine
    as diskless with it.

---
+++ tests/test_setup_bootable.py
@@ -0,0 +1,205 @@
+
+import os
+import pexpect
+import pytest
+import subprocess
+import sys
+
+@pytest.mark.parametrize('bootmode', ['bios', 'UEFI'])
+@pytest.mark.parametrize('numdisks', [1])
+@pytest.mark.parametrize('disktype', ['virtio', 'ide', 'nvme', 'usb'])
+@pytest.mark.parametrize('fstype', ['vfat']) # setup-bootable only support vfat so far
+def test_setup_bootable(qemu, alpine_conf_iso, disktype, bootmode, fstype):
+    if bootmode == 'UEFI' and disktype == 'nvme':
+        pytest.skip("UEFI does not boot from nvme")
+
+    qemu_args = qemu.machine_args + [
+            '-nographic',
+            '-m', '512M',
+            '-smp', '2',
+        ]
+
+    for img in qemu.images:
+        driveid = os.path.splitext(os.path.basename(img))[0]
+        if disktype == 'nvme':
+            qemu_args.extend([
+                '-drive', f'if=none,id={driveid},format=raw,file={img}',
+                '-device', f'nvme,serial={driveid},drive={driveid}'
+                ])
+        elif disktype == 'usb':
+            qemu_args.extend([
+                '-drive', f'if=none,id={driveid},format=raw,file={img}',
+                '-device', 'qemu-xhci',
+                '-device', f'usb-storage,drive={driveid}',
+                ])
+        else:
+            qemu_args.extend([ '-drive', f'if={disktype},format=raw,file={img}'])
+
+    if bootmode == 'UEFI':
+        qemu_args.extend(['-drive', 'if=pflash,format=raw,readonly=on,file='+qemu.uefi_code])
+
+    alpine_conf_args=[]
+    if alpine_conf_iso != None:
+        alpine_conf_args = ['-drive', 'media=cdrom,readonly=on,file='+alpine_conf_iso]
+
+    p = pexpect.spawn(qemu.prog, qemu_args + alpine_conf_args + [
+            '-kernel', qemu.boot['kernel'],
+            '-initrd', qemu.boot['initrd'],
+            '-append', 'quiet console='+qemu.console,
+            '-cdrom', qemu.boot['iso'],
+            ])
+
+#    p.logfile = sys.stdout.buffer
+
+    p.expect("login:", timeout=30)
+    p.send("root\n")
+
+    p.timeout = 2
+    p.expect("localhost:~#")
+
+    if alpine_conf_iso != None:
+        p.send("mkdir -p /media/ALPINECONF && mount LABEL=ALPINECONF /media/ALPINECONF && cp -r /media/ALPINECONF/* / && echo OK\n")
+        p.expect("OK")
+        p.expect("localhost:~#")
+
+    p.send("setup-interfaces -a -r && setup-apkrepos -1\n")
+    p.expect("localhost:~#", timeout=10)
+
+    devs = {'virtio': ['/dev/vda', '/dev/vda1'],
+            'ide': ['/dev/sda', '/dev/sda1'],
+            'nvme': ['/dev/nvme0n1', '/dev/nvme0n1p1'],
+            'usb': ['/dev/sda', '/dev/sda1'],
+            }
+
+    dev = devs[disktype]
+    disk = dev[0]
+    partition = dev[1]
+
+    if bootmode == 'bios':
+        p.send("fdisk "+disk+"\n")
+        p.expect("Command \\(m for help\\):")
+        p.send("m\n")
+        p.send("n\n")
+        p.expect("Partition type")
+        p.send("p\n")
+        p.expect("Partition number .*:")
+        p.send("1\n")
+        p.expect("First sector .*: ")
+        p.send("\n")
+        p.expect("Last sector or .*:")
+        p.send("\n")
+        p.expect("Command \\(m for help\\):")
+        p.send("a\n")
+        p.expect("Partition number .*:")
+        p.send("1\n")
+        p.expect("Command \\(m for help\\):")
+        p.send("w\n")
+    else:
+        p.send("apk add sfdisk\n")
+        p.expect("localhost:~#", timeout=10)
+        p.send("echo ',,U,*' | sfdisk --label gpt "+disk+"\n")
+        p.expect("localhost:~#")
+
+    p.send("mkfs."+fstype+" "+partition+" && echo OK\n")
+    p.expect("OK")
+
+    p.send("modprobe "+fstype+"\n")
+    p.expect("localhost:~#")
+
+    p.send("setup-bootable /media/cdrom "+partition+" && echo OK\n")
+    p.expect("OK", timeout=10)
+    p.send("mount -t "+fstype+" "+partition+" /mnt\n")
+    p.send(f"sed -i -e 's/^APPEND.*/APPEND console={qemu.console}/' /mnt/boot/syslinux/syslinux.cfg\n")
+    p.send("cat /mnt/boot/syslinux/syslinux.cfg\n")
+    p.send("umount /mnt\n")
+    p.send("poweroff\n")
+    p.expect(pexpect.EOF, timeout=10)
+
+    # boot the generated image
+    p = pexpect.spawn(qemu.prog, qemu_args + alpine_conf_args)
+#    p.logfile = sys.stdout.buffer
+
+    p.expect("login:", timeout=30)
+    p.send("root\n")
+
+    p.timeout = 2
+    p.expect("localhost:~#")
+
+    if alpine_conf_iso != None:
+        p.send("mkdir -p /media/ALPINECONF && mount LABEL=ALPINECONF /media/ALPINECONF && cp -r /media/ALPINECONF/* / && echo OK\n")
+        p.expect("OK")
+        p.expect("localhost:~#")
+
+    p.send("setup-alpine\n")
+
+    p.expect_exact("Select keyboard layout: [none] ")
+    p.send("\n")
+
+    hostname = "alpine"
+    p.expect("Enter system hostname \\(.*\\) \\[.*\\] ")
+    p.send(hostname+"\n")
+
+    p.expect("Which one do you want to initialize\\?.*\\[eth0\\] ")
+    p.send("\n")
+
+    p.expect("Ip address for eth0\\?.*\\[.*\\] ")
+    p.send("dhcp\n")
+
+    p.expect("Do you want to do any manual network configuration\\? \\(y/n\\) \\[n\\] ")
+    p.send("\n")
+
+    password = 'testpassword'
+    p.expect("New password: ", timeout=20)
+    p.send(password+"\n")
+
+    p.expect("Retype password: ")
+    p.send(password+"\n")
+
+    p.expect("Which timezone.*\\[UTC\\] ")
+    p.send("\n")
+
+    p.expect("HTTP/FTP proxy URL\\?.* \\[none\\] ", timeout=10)
+    p.send("\n")
+
+    i = p.expect(["Enter mirror number \\(.*\\) or URL to add \\(.*\\) \\[1\\] ",
+                  "Which NTP client to run\\? \\(.*\\) \\[.*\\] "], timeout=30)
+    if i==1:
+        p.send("\n")
+        p.expect("Enter mirror number \\(.*\\) or URL to add \\(.*\\) \\[1\\] ", timeout=30)
+    p.send("\n")
+
+    p.expect("Which SSH server\\? \\(.*\\) \\[openssh\\] ", timeout=20)
+    p.send("\n")
+
+    p.expect("Which disk\\(s\\) would you like to use\\? \\(.*\\) \\[none\\] ")
+    p.send("none\n")
+
+    p.expect("Enter where to store configs \\(.*\\) \\[.*\\] ")
+    p.send("\n")
+
+    p.expect("Enter apk cache directory \\(.*\\) \\[.*\\] ")
+    p.send("\n")
+
+    p.expect(hostname+":~#")
+    p.send("lbu commit\n")
+
+    p.expect(hostname+":~#")
+    p.send("poweroff\n")
+    p.expect(pexpect.EOF, timeout=10)
+
+    p = pexpect.spawn(qemu.prog, qemu_args)
+
+    p.expect("login:", timeout=60)
+    p.send("root\n")
+
+    p.expect("Password:", timeout=3)
+    p.send(password+"\n")
+
+    p.expect(hostname+":~#", timeout=3)
+    p.send("poweroff\n")
+    p.expect(pexpect.EOF, timeout=20)
+
+    for img in qemu.images:
+        os.unlink(img)
+
+
